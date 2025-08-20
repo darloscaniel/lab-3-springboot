0b89ed19650e1d62310c76cdcbb9fd73385adc98
Carlos Daniel <carlosdanielalves2006@gmail.com>
2025-08-20 18:59:00 -0300

    first commit

---
+++ src/test/java/com/example/lab_3_springboot/Lab3SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_3_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
